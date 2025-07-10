c42e773e1a08c376b9184a4a61b82057b3a92ca2
rubyj13 <134155557+rubyj13@users.noreply.github.com>
2025-07-10 11:40:05 -0400

    Update wp-config.php

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -155,3 +155,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+define('WP_HOME', 'https://picositahub.mywire.org/');
+define('WP_SITEURL', 'https://picositahub.mywire.org/');
