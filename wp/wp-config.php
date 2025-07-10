e8a9968c842ea3cb1b67da786d8537c324a91423
rubyj13 <134155557+rubyj13@users.noreply.github.com>
2025-07-09 22:44:39 -0400

    Update wp-config.php

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -40,7 +40,7 @@ if (isset($_ENV['HOST'])) {
 }
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4_general_ci' );
 
 /** The database collate type. Don't change this if in doubt. */
 if (isset($_ENV['DB_COLLATE'])) {
